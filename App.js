e109a829c3393cef644d2ecd455527a70d0f82fc
Cosmos <Alfredchrisayo@gmail.com>
2023-12-05 23:17:11 +0000

    add client side

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 const { urlencoded } = require("body-parser");
 const port = process.env.PORT || 2201;
 const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 const { getDailyDecisionTips } = require("./Api/tips");
-
+app.get("/api/", (req, res) => {
+  res.sendFile(__dirname, "dist", "index.html");
+});
 app.post("/api/subscribe/tips", getDailyDecisionTips);
 app.listen(port, function () {
   console.log(`Server running on live port ${port}`);
--- client/HT/src/App.jsx
+++ client/HT/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-blue-600 h-screen flex justify-center items-center flex-col">
+        <h1 className="text-gray-300 text-4xl font-bold">
+          {" "}
+          Clinical Decision Support System
+        </h1>
+        <div className="flex w-96 flex-col border border-blue-600 ">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            className="w-full border border-slate-100 px-2 py-2"
+          />
+          <input
+            type="text"
+            placeholder="What is your ailment "
+            className="w-full border border-slate-100 px-2 py-2"
+          />
+          <input
+            type="submit"
+            value="subscribe"
+            className="w-full border border-slate-100 px-2 py-2 hover:bg-blue-500 text-white"
+          />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
